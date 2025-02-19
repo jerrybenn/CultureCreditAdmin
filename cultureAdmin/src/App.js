e8e4188e49913da37a9432e6da94516feef3770a
Jerry Benn <jerrydarobenn@gmail.com>
2025-02-19 10:59:20 -0500

    saving progress

--- cultureAdmin/src/App.js
+++ cultureAdmin/src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 
 import Home from './pages/homepage/Home';
 import Login from './pages/loginFolder/Login';
@@ -9,13 +9,12 @@ import Login from './pages/loginFolder/Login';
 
 function App() {
   return (
-   
-    <Router>
+    
       <Routes>
         <Route path="/" element={<Login />} />
         <Route path="/home" element={<Home />} />
       </Routes>
-    </Router>
+
   )
 }
 
