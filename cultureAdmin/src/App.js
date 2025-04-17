054fac4cfb1f3e2344e75b62b43e2f1d5052154e
Jerry Benn <jerrydarobenn@gmail.com>
2025-04-17 15:45:14 -0400

    made a adminPage

--- cultureAdmin/src/App.js
+++ cultureAdmin/src/App.js
@@ -1,14 +1,15 @@
-import React from 'react';
-import './App.css';
-import { BrowserRouter, Routes, Route, useLocation } from 'react-router-dom';
+import React from "react";
+import "./App.css";
+import { BrowserRouter, Routes, Route, useLocation } from "react-router-dom";
 
-import Home from './pages/homepage/Home';
-import Navbar from './components/navbarFolder/Navbar';
-import Login from './pages/loginFolder/Login';
-import Events from './pages/eventsPage/Events';
-import Calender from './pages/calenderPage/Calender';
-import Students from './pages/studentsPage/Students';
-import Instructor from './pages/instructorPage/Instructor';
+import Home from "./pages/homepage/Home";
+import Navbar from "./components/navbarFolder/Navbar";
+import Login from "./pages/loginFolder/Login";
+import Events from "./pages/eventsPage/Events";
+import Calender from "./pages/calenderPage/Calender";
+import Students from "./pages/studentsPage/Students";
+import Instructor from "./pages/instructorPage/Instructor";
+import AddAdmin from "./pages/addAdminPage/AddAdmin";
 
 function App() {
   return (
@@ -19,19 +20,31 @@ function App() {
 }
 
 function MainContent() {
-  const location = useLocation(); 
-  const showNavbar = location.pathname !== "/";
+  const location = useLocation();
+  const showNavbar =
+    location.pathname !== "/" && location.pathname !== "/addAdmin";
   return (
     <div className="appContainer">
       {showNavbar && <Navbar />}
-      <div className="mainContent" style={{ marginLeft: showNavbar ? "220px" : "0px" }}>
+      <div
+        className="mainContent"
+        style={{
+          marginLeft:
+            location.pathname === "/addAdmin"
+              ? "0px"
+              : showNavbar
+              ? "220px"
+              : "0px",
+        }}
+      >
         <Routes>
           <Route path="/" element={<Login />} />
           <Route path="/home" element={<Home />} />
-          <Route path= "/events" element={<Events />} />
-          <Route path= "/calender" element={<Calender/>} />
-          <Route path= "/students" element={<Students/>} />
-          <Route path= "/instructor" element= {<Instructor/>} />
+          <Route path="/events" element={<Events />} />
+          <Route path="/calender" element={<Calender />} />
+          <Route path="/students" element={<Students />} />
+          <Route path="/instructor" element={<Instructor />} />
+          <Route path="/addAdmin" element={<AddAdmin />} />
         </Routes>
       </div>
     </div>
---
+++ cultureAdmin/src/pages/addAdminPage/AddAdmin.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import HorizontalNav from '../../components/horizontalNavbar/HorizontalNav.jsx';
+
+const AddAdmin = () => {
+  return (
+
+        <div>
+            <div className="mainContent">
+                <h1>Add Admin</h1>
+            </div>
+        </div>
+  );
+};
+
+export default AddAdmin;
